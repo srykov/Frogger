3cd7c236e431e6e3ff33ef7dcff2914b1355c7be
Sara Rykov <sara.rykov@gmail.com>
2018-01-15 14:26:39 -0500

    feature: initial code to allow initial rendering of game to complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x = 0, y = 0, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +28,35 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x = 202, y = 405){
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+};
 
+Player.prototype.update = function(dt){
+
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(){
+    console.log("Handle Input");
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const numEnemies = 5;
+const allEnemies = [];
+for (let i = 0; i < numEnemies; i++){
+    const enemy = new Enemy();
+    allEnemies.push(enemy);
+}
 
+const player = new Player(202,405);
 
 
 // This listens for key presses and sends the keys to your
